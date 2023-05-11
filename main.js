e0972f3fcc34b2f7fa6599e13e90cd49222f0754
Отдел допечатной подготовки <ctp@lazurprint.ru>
2023-05-11 17:47:03 +0500

    add catalog

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-let tabsBtn = document.querySelectorAll('.list-group-item');
-let tabsItem = document.querySelectorAll('.products');
-let tabsItemProducts = document.querySelectorAll('.row__products');
-let tabsBtnProducts = document.querySelectorAll('.btn__products');
+const tabsBtn = document.querySelectorAll('.list-group-item');
+const tabsItem = document.querySelectorAll('.products');
+const tabsItemProducts = document.querySelectorAll('.row__products');
+const tabsBtnProducts = document.querySelectorAll('.btn__products');
+const product = document.querySelectorAll('.product');
+const productImg = document.querySelectorAll('.product-img');
+const productText = document.querySelectorAll('.product__text');
+// const productTitle = document.querySelectorAll('.product-title');
+const productDesc = document.querySelectorAll('.product-desc');
+const modal = document.querySelector('.modal');
+const modalItem = document.querySelector('.modal__item');
+const modalItemImg = document.querySelector('.modal__item-img');
+const modalItemDescr = document.querySelector('.modal__item-descr');
+const close = document.querySelector('.close');
+
 
 tabsBtn.forEach(function (element) {
     element.addEventListener('click', function (e) {
@@ -13,15 +24,15 @@ tabsBtn.forEach(function (element) {
         tabsItem.forEach(function (element) { element.classList.remove('active') });
         document.querySelector(`[data-target="${path}"]`).classList.add('active');
         if (element.textContent === 'Книги') {
-            tabsItemProducts[0].classList.add('row--active')
+            document.querySelector('.tab1').classList.add('row--active')
         } else if (element.textContent === 'Журналы') {
-            tabsItemProducts[4].classList.add('row--active')
+            document.querySelector('.tab5').classList.add('row--active')
         } else if (element.textContent === 'Брошюры') {
-            tabsItemProducts[8].classList.add('row--active')
+            document.querySelector('.tab9').classList.add('row--active')
         } else if (element.textContent === 'Листовки') {
-            tabsItemProducts[12].classList.add('row--active')
+            document.querySelector('.tab13').classList.add('row--active')
         } else if (element.textContent === 'Сувенирная продукция') {
-            tabsItemProducts[16].classList.add('row--active')
+            document.querySelector('.tab17').classList.add('row--active')
         }
 
     });
@@ -30,8 +41,10 @@ tabsBtn.forEach(function (element) {
 tabsBtnProducts.forEach(function (element) {
     element.addEventListener('click', function (e) {
         const path = e.currentTarget.dataset.path;
-
-        tabsBtnProducts.forEach(function (btn) { btn.classList.remove('btn__row--active') });
+        
+        tabsBtnProducts.forEach(function (btn) {
+            btn.classList.remove('btn__row--active')
+        });
         e.currentTarget.classList.add('btn__row--active');
 
         tabsItemProducts.forEach(function (element) { element.classList.remove('row--active') });
@@ -39,4 +52,34 @@ tabsBtnProducts.forEach(function (element) {
 
     });
 });
+
+product.forEach((item) => {
+    item.addEventListener('click', function () {
+        item.classList.add('product--active')
+        modal.classList.add('modal--active')
+        item.children[1].children[1].classList.add('product-desc--active')
+        // productImg.classList.add('product-img--active')
+        // productText.classList.add('product__text--active')
+        console.log(item.children[0].innerHTML);
+        modalItemImg.innerHTML = item.children[0].innerHTML
+        modalItemDescr.innerHTML = item.children[1].innerHTML
+    })
+});
+
+close.addEventListener('click', ()=> {
+    modal.classList.remove('modal--active')
+    productDesc.forEach(el => {
+        el.classList.remove('product-desc--active')
+    })
+})
+
+modal.addEventListener('click',(e)=> {
+    if (e.target === modal) {
+        modal.classList.remove('modal--active')
+        productDesc.forEach(el => {
+            el.classList.remove('product-desc--active')
+        })
+    }
+})
+
 
